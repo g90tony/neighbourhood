2e3c667f617a9113c0818405e8985eef59f1513e
Caleb Tony <calebmbugua@gmail.com>
2021-06-06 08:36:42 +0300

    completed neighbourhood model

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.CharField()
+    neighborhood_location = models.CharField
+    occupants_count = models.IntegerField(default=0)
+    admin_foreign_key = models.ForeignKey(User)
+    
+    def create_neigborhood(self):
+        self.save()
+
+    def delete_neigborhood(self):
+        self.delete()
+
+    def find_neigborhood(self, neighbourhood_id):
+        return self.objects.filter(id = neighbourhood_id).first()
+
+    def update_neighborhood(self):
+        self.save()
+
+    def update_occupants(self, new_count):
+        self.occupants_count = new_count
+        self.save()
