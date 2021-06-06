1115889c78da300f1f5431c4fc6fe4cc726ec33d
Caleb Tony <calebmbugua@gmail.com>
2021-06-06 09:04:11 +0300

    completed models

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,21 +1,21 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 class Neighbourhood(models.Model):
     neighbourhood_name = models.CharField()
-    neighborhood_location = models.CharField
+    neighborhood_location = models.CharField()
     occupants_count = models.IntegerField(default=0)
-    admin_foreign_key = models.ForeignKey(User)
+    admin_foreign_key = models.ForeignKey(User, on_delete=models.CASCADE)
     
-    def create_neigborhood(self):
+    def create_neighbourhood_(self):
         self.save()
 
-    def delete_neigborhood(self):
+    def delete_neighbourhood_(self):
         self.delete()
 
-    def find_neigborhood(self, neighbourhood_id):
-        return self.objects.filter(id = neighbourhood_id).first()
+    def find_neighbourhood(self, neighbourhood_id):
+        return self.objects.filter(id = neighbourhood_id).all()
 
     def update_neighborhood(self):
         self.save()
@@ -23,3 +23,38 @@ class Neighbourhood(models.Model):
     def update_occupants(self, new_count):
         self.occupants_count = new_count
         self.save()
+
+class Profile(models.Model):
+    names = models.CharField()
+    neighborhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    avatar = CloudinaryField('image', default=None)
+    user = models.ForeignKey(User)
+    
+class Business(models.Model):
+    business_name = models.CharField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighborhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    Business_email = models.EmailField()
+    
+    def create_business(self):
+        self.save()
+        
+    def delete_business(self):
+        self.delete()
+        
+    def find_business(self, business_id):
+        return self.objects.filter(id = business_id).all()
+    
+    def update_business(self):
+        self.save()
+        
+class TextPost(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    create_on = models.DateField(auto_now_add=True)
+    text_content = models.TextField()
+        
+class ImagePost(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    create_on = models.DateField(auto_now_add=True)
+    text_content = models.TextField()
+    image = CloudinaryField('image', default=None)
