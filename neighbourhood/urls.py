01aec6440867870c89245af8e4256f2ac8b70632
Caleb Tony <calebmbugua@gmail.com>
2021-06-08 05:48:31 +0300

    minor syntaxt corrections

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='Home'),
-    path('/events', views.events, name='All Events'),
-    path('/events/upcoming', views.upcoming_events, name='Upcoming Events'),
-    path('/events/past', views.past_events, name='Past Events'),
-    path('/businesses', views.businesses, name='All Businesses'),
-    path('/businesses/search', views.busines_search, name='Businesses Search'),
+    path('profile/create', views.create_profile, name='Create Profile'),
+    path('events', views.events, name='All Events'),
+    path('events/upcoming', views.upcoming_events, name='Upcoming Events'),
+    path('events/past', views.past_events, name='Past Events'),
+    path('businesses', views.businesses, name='All Businesses'),
+    path('businesses/search', views.busines_search, name='Businesses Search'),
 ]
