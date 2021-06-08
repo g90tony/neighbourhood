d5bc3e0cf07ad168a52ccea74e3d97a24dd608d2
Caleb Tony <calebmbugua@gmail.com>
2021-06-08 05:47:44 +0300

    registered all models

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Business, Contact, Event, ImagePost, Neighbourhood, Profile, TextPost
 # Register your models here.
+
+admin.site.register(Business)
+admin.site.register(Contact)
+admin.site.register(Event)
+admin.site.register(ImagePost)
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(TextPost)
