98928cca8e82db7b12d641753cf64e96f297b1eb
Caleb Tony <calebmbugua@gmail.com>
2021-06-08 03:52:06 +0300

    added necessary routes

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='HOME'),
+    path('', views.index, name='Home'),
+    path('/events', views.events, name='All Events'),
+    path('/events/upcoming', views.upcoming_events, name='Upcoming Events'),
+    path('/events/past', views.past_events, name='Past Events'),
+    path('/businesses', views.businesses, name='All Businesses'),
+    path('/businesses/search', views.busines_search, name='Businesses Search'),
 ]
